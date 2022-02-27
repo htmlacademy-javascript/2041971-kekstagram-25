a0095b94243005c8528768e21807ce329b09bf15
Лариса Зубкова <larissazubkova@gmail.com>
2022-02-27 19:56:20 +0300

    Implement task

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 /*Функция, возвращающая случайное целое число из переданного диапазона включительно.
 Источник:https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#получение_случайного_целого_числа_в_заданном_интервале_включительно*/
-function getRandomeInInclusie (min, max){
+function getRandomeInInclusie(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (max>min && min >=0){
-    return Math.floor(Math.random()*(max-min+1))+min;
-  } return false;
+  if (max > min && min >= 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return false;
 }
-getRandomeInInclusie(50,3);
+getRandomeInInclusie(50, 3);
 
 //Функция для проверки максимальной длины строки.
-function getComment(comment, max){
-  if (comment.length<=max){
-    return true;
-  }return false;
+function checkMaxStrLength(str, maxLenght) {
+  return str.length <= maxLenght;
 }
-getComment('larissa',10);
-
+checkMaxStrLength('larissa', 10);
