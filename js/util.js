d54207d36e4dab7db223727f2e692cf007b67e33
Лариса Зубкова <larissazubkova@gmail.com>
2022-03-11 21:23:51 +0300

    Подправила функцию

--- js/util.js
+++ js/util.js
@@ -6,6 +6,7 @@ function getRandomeInInclusie(min, max) {
   if (max > min && min >= 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  return 0;
 }
 
 //Функция для проверки максимальной длины строки.
