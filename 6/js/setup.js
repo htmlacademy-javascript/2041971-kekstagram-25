3700fed7fe4e277e73b24fad9c2f04221d28b5f9
Keks <keks@htmlacademy.ru>
2022-03-16 12:05:52 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/setup.js
@@ -0,0 +1,60 @@
+import { picturesData } from './popup.js';
+const showBigPicture = () => {
+  const bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
+
+  const photoTemplate = picturesData[0];
+  window.console.log(photoTemplate);
+};
+window.console.log(showBigPicture);
+
+// const bodyElements = document.querySelector('body');
+
+// const bigImg = openBigPicture('.big-picture__img');
+// const likesCount =openBigPicture('.likes-count');
+// const commentsCount = openBigPicture('.comments-count');
+// //const socialComments = openBigPicture.querySelector('.social__comments');
+// const socialCommentCount = openBigPicture.querySelector('.social__comment-count');
+// const commentsLoader = openBigPicture.querySelector('.comments-loader');
+
+// const buttonCancel = openBigPicture.querySelector('.big-picture__cancel');
+
+// // const createCommentTemplate = ({avatar, name, message})=>(
+// //    <li class="social__comment">
+// //     <img
+// //         class="social__picture"
+// //         src="${avatar}"
+// //         alt="${name}"
+// //         width="35" height="35">
+// //     <p class="social__text">"${message}"</p>
+// // </li>
+// // )
+
+// const isEscPress = (evt)=>(evt.keyCode===27);
+// const isMouseClick =(evt)=>(evt.type==='click');
+
+// const hideBigPicture = (evt)=>{
+//   evt.preventDefault();
+//   if (isEscPress(evt) || isMouseClick(evt)) {
+//     openBigPicture.classList.add('hidden');
+//     bodyElements.classList.remove('modal-open');
+
+//     buttonCancel.addEventListener('click', hideBigPicture);
+//   }
+// };
+
+// const showBigPicture = ({url,likes,comments})=>{
+//   bigImg.srs = url;
+//   likesCount.textContent = likes;
+//   commentsCount.textContent = comments.length;
+//   openBigPicture.classList.remove('hidden');
+//   socialCommentCount.classList.add('hidden');
+//   commentsLoader.classList.add('hidden');
+//   bodyElements.classList.add('modal-open');
+
+//   buttonCancel.addEventListener('click', hideBigPicture);
+// };
+
+
+// window.console.log(showBigPicture);
+
