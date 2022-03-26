8f5bdfd37c2db9929ce4cd4275953f8ab9eed6fc
Лариса Зубкова <larissazubkova@gmail.com>
2022-03-26 10:57:57 +0300

    Функция закрытия фото

--- js/setup.js
+++ js/setup.js
@@ -4,15 +4,16 @@ const showBigPicture = (pictureElement) => {
   const bigPicture = document.querySelector('.big-picture');
   bigPicture.classList.remove('hidden');
 
-  const commentTemplate = () => `<li class="social__comment">
+  const commentTemplate = (comment) => (`<li class="social__comment">
   <img
       class="social__picture"
-      src="${pictureElement.comments.avatar}"
-      alt="${pictureElement.comments.name}"
+      src="${comment.avatar}"
+      alt="${comment.name}"
     width="35" height="35">
- <p class="social__text">"${pictureElement.comments.message}"</p>
-</li>`;
-  window.console.log(commentTemplate);
+ <p class="social__text">"${comment.message}"</p>
+</li>`);
+
+  window.console.log(pictureElement);
 
   const bigImg = bigPicture.querySelector('.big-picture__img');
   const likesCount = bigPicture.querySelector('.likes-count');
@@ -24,7 +25,7 @@ const showBigPicture = (pictureElement) => {
   likesCount.textContent = pictureElement.likes;
   commentsCount.textContent = pictureElement.comments.length;
   socialCaption.textContent = pictureElement.description;
-  socialComments.innerHTML = '';
+  socialComments.innerHTML = commentTemplate(pictureElement.comments[0]);
 
   const socialCommentCount = bigPicture.querySelector('.social__comment-count');
   const commentsLoader = bigPicture.querySelector('.comments-loader');
@@ -32,52 +33,27 @@ const showBigPicture = (pictureElement) => {
   commentsLoader.classList.add('hidden');
 };
 showBigPicture(picturesData[0]);
+window.console.log(picturesData);
 
 
-// const bodyElements = document.querySelector('body');
+const bodyElements = document.querySelector('body');
+bodyElements.classList.add('modal-open');
+window.console.log(bodyElements);
+const isEscPress = (evt)=>(evt.keyCode===27);
+const isMouseClick =(evt)=>(evt.type==='click');
 
-
-// const buttonCancel = openBigPicture.querySelector('.big-picture__cancel');
-
-
-// const isEscPress = (evt)=>(evt.keyCode===27);
-// const isMouseClick =(evt)=>(evt.type==='click');
-
-// const hideBigPicture = (evt)=>{
-//   evt.preventDefault();
-//   if (isEscPress(evt) || isMouseClick(evt)) {
-//     openBigPicture.classList.add('hidden');
-//     bodyElements.classList.remove('modal-open');
-
-//     buttonCancel.addEventListener('click', hideBigPicture);
-//   }
-// };
-
-// const showBigPicture = ()=>{
-//   socialCommentCount.classList.add('hidden');
-//   commentsLoader.classList.add('hidden');
-//   bodyElements.classList.add('modal-open');
-
-//   buttonCancel.addEventListener('click', hideBigPicture);
-// };
-
-
-// window.console.log(showBigPicture);
-
-//import {createComment} from './data.js';
-
-//const comment = createComment();
-/*const createCommentTemplate = (commentData) => {
-  const commentTemplate = `<li class="social__comment">
-     <img
-         class="social__picture"
-         src="${commentData.avatar}"
-         alt="${commentData.name}"
-       width="35" height="35">
-    <p class="social__text">"${commentData.message}"</p>
-</li>`;
-  window.console.log(commentData);
-  return commentTemplate;
+const hideBigPicture = (evt)=>{
+  evt.preventDefault();
+  if (isEscPress(evt) || isMouseClick(evt)) {
+    const bigPicture = document.querySelector('.big-picture');
+    bigPicture.classList.add('hidden');
+    bodyElements.classList.remove('modal-open');
+  }
 };
-const template = createCommentTemplate();
-//window.console.log(template);*/
+
+const module = ()=>{
+  const buttonCancel = document.querySelector('.big-picture__cancel');
+  buttonCancel.addEventListener('click', hideBigPicture);
+};
+
+module();
